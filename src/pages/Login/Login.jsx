22e6d2c3c06f4e1bbdf4c8f51450da402d7b44ce
Anayet Abir <anayetabir007@gmail.com>
2024-03-28 03:25:09 +0600

    Service bug fixed and run successfully

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -26,13 +26,13 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
@@ -43,7 +43,7 @@ const Login = () => {
                         </div>
                     </form>
 
-                    <p className='my-4 text-center'>New to Car Doctors <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
+                    <p className='my-4 text-center'>New to Car Doctors? <Link className='text-orange-600 font-bold' to="/signup">Sign Up</Link></p>
                 </div>
             </div>
         </div>
