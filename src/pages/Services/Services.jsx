c44cb6d58df77c6d334511b1daa27e036d970e2b
Anayet Abir <anayetabir007@gmail.com>
2024-03-27 13:39:10 +0600

    firebase

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,6 +1,23 @@
-import React from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
+import ServiceCard from "./serviceCard";
+
 
 const Services = () => {
+
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+
+    }, [])
+
+
+
+
+
     return (
         <div className='mt-4'>
             <div className='text-center'>
@@ -8,6 +25,16 @@ const Services = () => {
                 <h2 className='text-5xl'>Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do  not look even slightly believable. </p>
             </div>
+            <div>
+                {/* {
+                    services.map(service => <serviceCard>
+                        key={service._id}
+                        servce={service}
+                    </serviceCard>)
+                } */}
+
+            </div>
+
         </div>
     );
 };
